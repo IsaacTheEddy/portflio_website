7d1ce94449bf8b7304a74fd47ae348f6c8e2efa8
Isaac Edwards <isaactheedwards@gmail.com>
2025-09-27 13:20:06 -0500

    Getting the Portfolio section started

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import ContentSection from "@/components/ContentSection";
+import Portfolio from "@/components/PortfolioSection";
 import IsaacPic from "@/public/assets/Isaac_Edwards_1_Formatted.jpg";
 import { useRef } from "react";
 import { useGSAP } from "@gsap/react";
@@ -51,29 +52,56 @@ export default function Home() {
         occaecat cupidatat non proident, sunt in culpa qui officia deserunt
         mollit anim id est laborum.
       </ContentSection>
-      <div ref={missionSectionRef}>
-        <ContentSection title="My Mission" imageUrl={IsaacPic.src} reverse={true}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-          velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-          occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-          mollit anim id est laborum.
+      <div id="mission" ref={missionSectionRef}>
+        <ContentSection
+          title="My Mission"
+          imageUrl={IsaacPic.src}
+          reverse={true}
+        >
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
         </ContentSection>
       </div>
-      <section id="portfolio" ref={portfolioRef} className="h-screen bg-gray-800 text-white flex items-center justify-center">
-        <h2 className="text-4xl font-bold">Portfolio</h2>
+      <section
+        id="portfolio"
+        ref={portfolioRef}
+        className="h-screen opacity-90 bg-gray-800 text-white flex items-center justify-center"
+      >
+        <Portfolio />
       </section>
 
-      {/* Floating Bottom NAvigation BAr */}
-      <nav className="fixed bottom-4 left-1/2 transform -translae-x-1/22 p-2 bg-gray-800 rounded-full shadow-2xl z-50">
-        <ul className="flex space-x-2 md:space-x-4">
-          {["home", "about", "work", "contact"].map((section) => (
-            <li key={section}>
-              <button></button>
-            </li>
-          ))}
+      {/* Floating Bottom Navigation Bar */}
+      <nav className="fixed bottom-4 left-1/2 transform -translate-x-1/2 p-4 bg-gray-800 rounded-full shadow-2xl z-50">
+        <ul className="flex space-x-4">
+          <li>
+            <button
+              onClick={() => scrollToSection("home")}
+              className="text-white hover:text-gray-300"
+            >
+              Home
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={() => scrollToSection("mission")}
+              className="text-white hover:text-gray-300"
+            >
+              Mission
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={() => scrollToSection("portfolio")}
+              className="text-white hover:text-gray-300"
+            >
+              Portfolio
+            </button>
+          </li>
         </ul>
       </nav>
     </>
---
+++ components/PortfolioSection.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Portfolio() {
+  return (
+    <>
+      <h2 className="text-4xl font-bold">Portfolio</h2>
+    </>
+  );
+}
